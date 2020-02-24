0433e76d4ff584321084c3baa42569b4fc343a61
TJW-0 <61374195+TJW-0@users.noreply.github.com>
2020-02-24 10:47:20 +0800

    Add files via upload

---
+++ Homework for class 1/Project1/Project1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Project1
+{
+    class Calculator
+    {
+        public static double Calculate(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.Write("请输入一个非零数！");
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            Console.WriteLine("C#语言的控制台计算器程序\r");
+            Console.WriteLine("------------------------\n");
+            while (!endApp)
+            {
+                string num1 = "";
+                string num2 = "";
+                double result = 0;
+                Console.Write("请输入第一个操作数：");
+                num1 = Console.ReadLine();
+                double cleanNum1 = 0;
+                while (!double.TryParse(num1, out cleanNum1))
+                {
+                    Console.Write("请输入一个数字：");
+                    num1 = Console.ReadLine();
+                }
+                Console.Write("请输入第二个操作数（如果您想使用除法，请不要输入0）：");
+                num2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(num2, out cleanNum2))
+                {
+                    Console.Write("请输入一个数字：");
+                    num2 = Console.ReadLine();
+                }
+                Console.WriteLine("输入字母已选择对应的计算方法：");
+                Console.WriteLine("\ta - 加");
+                Console.WriteLine("\ts - 减");
+                Console.WriteLine("\tm - 乘");
+                Console.WriteLine("\td - 除");
+                string op = Console.ReadLine();
+                try
+                {
+                    result = Calculator.Calculate(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("这将会导致一个数学错误！\n");
+                    }
+                    else Console.WriteLine("运算结果: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("发生事故导致程序终止。\n - 细节: " + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+                Console.Write("输入“q”关闭计算器，或者输入其他数以继续进行其他运算：");
+                if (Console.ReadLine() == "q") endApp = true;
+                Console.WriteLine("\n");
+            }
+            return;
+        }
+    }
+}
---
+++ Homework for class 1/Project2/Project2/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project2
+{
+    public partial class Form1 : Form
+    {
+        double num1;
+        double num2;
+        bool c = false;
+        string d;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            d = this.comboBox1.Text;
+            c = true;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string str1 = this.NumBox1.Text.Trim();//第一个文本框的名字为Tex01第二个文本框为Tex02第三个框为Texresult
+            string str2 = this.NumBox2.Text.Trim();
+            if (str1.Length == 0)
+            {
+                MessageBox.Show("第一个数没有输入有效的数字", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.NumBox1.Text = string.Empty;
+                this.NumBox1.Focus();
+                return;
+            }
+            num1 = Convert.ToDouble(str1);
+            if (str2.Length == 0)
+            {
+                MessageBox.Show("第二个数没有输入有效的数字", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.NumBox2.Text = string.Empty;
+                this.NumBox2.Focus();
+                return;
+            }
+            num2 = Convert.ToDouble(str2);
+            if (c != true)
+            {
+                MessageBox.Show("还未选择运算符号", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            switch (d)
+            {
+                case "+":
+                    this.ResultBox.Text = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    this.ResultBox.Text = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    this.ResultBox.Text = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        MessageBox.Show("请不要将0作为除数", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        this.ResultBox.Text = (num1 / num2).ToString();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            this.NumBox1.Text = string.Empty;
+            this.NumBox2.Text = string.Empty;
+            this.ResultBox.Text = string.Empty;
+            this.NumBox1.Focus();
+        }
+    }
+}
