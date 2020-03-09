c69425b48684c863914f846198a954d3fe453cf1
TJW-0 <61374195+TJW-0@users.noreply.github.com>
2020-03-09 10:57:59 +0800

    Add files via upload

---
+++ Homework for class 4/Project1/Project1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node;
+            for (node = head; node != null;node = node.Next)
+                {
+                action(node.Data);
+                }
+        }
+    }
+    delegate void Fun(int x);
+    class Program
+    {
+        public void print(int n)
+        {
+            Console.WriteLine(n);
+        }
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            int sum = 0;
+            Console.WriteLine("链表元素如下：");
+            intlist.ForEach(x => Console.WriteLine(x));
+            int max = 0;
+            int min = 0;
+            int n = 0;
+            intlist.ForEach(max => { n = n < max ? max : n; } );
+            Console.WriteLine("链表元素最大值为" + n);
+            intlist.ForEach(min => { n = n > min ? min : n; });
+            Console.WriteLine("链表元素最小值为" + n);
+            intlist.ForEach(x => sum += x);
+            Console.WriteLine("链表元素之和为" + sum);
+        }
+
+    }
+}
---
+++ Homework for class 4/Project2/Project2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading;
+
+namespace Project2
+{
+    public delegate void TickHandler(object sender, TickEventArgs args);
+    public class TickEventArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+    public delegate void AlarmHandler(object sender, AlarmEventArgs args);
+    public class AlarmEventArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+    public class Clock
+    {
+        public event TickHandler OnTick;
+        public event AlarmHandler OnAlarm;
+        public void Tick()
+        {
+            TickEventArgs args = new TickEventArgs();
+            OnTick(this, args);
+        }
+        public void Alarm()
+        {
+            AlarmEventArgs args = new AlarmEventArgs();
+            OnAlarm(this, args);
+        }
+    }
+    public class SetClock
+    {
+        public int setHour { get; set; }
+        public int setMinute { set; get; }
+        public int setSecond { get; set; }
+        public Clock clock = new Clock();
+        public SetClock(int h, int m, int s)
+        {
+            setHour = h;
+            setMinute = m;
+            setSecond = s;
+            clock.OnTick += new TickHandler(Clk_OnTick);
+            clock.OnAlarm += new AlarmHandler(Clk_OnAlarm);
+        }
+        void Clk_OnTick(object sender, TickEventArgs args)
+        {
+            Console.WriteLine("滴答");
+        }
+        void Clk_OnAlarm(object sender, AlarmEventArgs args)
+        {
+            Console.WriteLine("叮叮叮");
+        }
+        public void work()
+        {
+            while (true)
+            {
+                int hou = DateTime.Now.Hour;
+                int min = DateTime.Now.Minute;
+                int sec = DateTime.Now.Second;
+                if (hou == setHour & min == setMinute & sec == setSecond)
+                {
+                    clock.Alarm();
+                    break;
+                }
+                else
+                {
+                    clock.Tick();
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请设置响铃时间：");
+            Console.WriteLine("时：");
+            int inputHour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("分：");
+            int inputMinute = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("秒：");
+            int inputSecond = Convert.ToInt32(Console.ReadLine());
+            SetClock setClock = new SetClock(inputHour, inputMinute, inputSecond);
+            setClock.work();
+        }
+    }
+}
