bf469bb70f78a86ba4b9feaaa5c34a445fc94e15
TJW-0 <61374195+TJW-0@users.noreply.github.com>
2020-03-30 11:28:51 +0800

    Add files via upload

---
+++ Homework for class 7/Homework for class 7/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Homework_for_class_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(109, 138);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(31, 21);
+            this.textBox13.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(0, 138);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(100, 21);
+            this.textBox7.TabIndex = 7;
+            this.textBox7.Text = "左分支角度th2";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(0, 111);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(100, 21);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.Text = "右分支角度th1";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(0, 84);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.Text = "左分支长度比per2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(0, 57);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "右分支长度比per1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "递归深度n";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(3, 30);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "主干长度leng";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(109, 111);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(31, 21);
+            this.textBox12.TabIndex = 12;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(109, 84);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(31, 21);
+            this.textBox11.TabIndex = 11;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(109, 57);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(31, 21);
+            this.textBox10.TabIndex = 10;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(109, 30);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(31, 21);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(109, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(31, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "选择颜色";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "绘制";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.textBox9);
+            this.panel2.Controls.Add(this.textBox10);
+            this.panel2.Controls.Add(this.textBox11);
+            this.panel2.Controls.Add(this.textBox12);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Controls.Add(this.textBox6);
+            this.panel2.Controls.Add(this.textBox7);
+            this.panel2.Controls.Add(this.textBox13);
+            this.panel2.Location = new System.Drawing.Point(641, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(158, 426);
+            this.panel2.TabIndex = 18;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(623, 426);
+            this.panel1.TabIndex = 17;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "重置";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Homework for class 7/Homework for class 7/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Homework_for_class_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = this.textBox2.Text.Trim();
+            if (str.Length == 0)
+            {
+                MessageBox.Show("没有输入有效的递归深度", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox2.Clear();
+                return;
+            }
+            n = Convert.ToInt32(textBox2.Text);
+            str = this.textBox9.Text.Trim();
+            if (str.Length == 0)
+            {
+                MessageBox.Show("没有输入有效的主干长度", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox9.Clear();
+                return;
+            }
+            leng = Convert.ToDouble(textBox9.Text);
+            str = this.textBox10.Text.Trim();
+            if (str.Length == 0)
+            {
+                MessageBox.Show("没有输入有效的右分支长度比", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox10.Clear();
+                return;
+            }
+            per1 = Convert.ToDouble(textBox10.Text);
+            str = this.textBox11.Text.Trim();
+            if (str.Length == 0)
+            {
+                MessageBox.Show("没有输入有效的左分支长度比", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox11.Clear();
+                return;
+            }
+            per2 = Convert.ToDouble(textBox11.Text);
+            str = this.textBox12.Text.Trim();
+            if (str.Length == 0)
+            {
+                MessageBox.Show("没有输入有效的右分支角度", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox12.Clear();
+                return;
+            }
+            th1 = Convert.ToDouble(textBox12.Text);
+            str = this.textBox13.Text.Trim();
+            if (str.Length == 0)
+            {
+                MessageBox.Show("没有输入有效的右分支角度", "系统提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox13.Clear();
+                return;
+            }
+            th2 = Convert.ToDouble(textBox13.Text);
+            if (graphics == null && color != Color.Empty) 
+            {
+                graphics = this.panel1.CreateGraphics();
+                drawCayleyTree(n, 200, 310, leng, -Math.PI / 2, color, th1, th2, per1, per2);
+                return;
+            }
+            else 
+            {
+                MessageBox.Show("已有图像！");
+                return; 
+            } ;
+            
+        }
+        private Graphics graphics = null;
+        int n = 0;
+        double leng = 0;
+        Color color = Color.Empty;
+        double th1 = 0;                                             
+        double th2 = 0;                                             
+        double per1 = 0;                                                 
+        double per2 = 0;                                                  
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th, Color color, double th1, double th2, double per1, double per2)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            drawLine(x0, y0, x1, y1, n/2);
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1, color, th1, th2, per1, per2);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2, color, th1, th2, per1, per2);
+        }
+        void drawLine(double x0, double y0, double x1, double y1, int width)
+        {
+            graphics.DrawLine(new Pen(color, width), (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.colorDialog1.ShowDialog();
+            color = this.colorDialog1.Color;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            graphics = null;
+            n = 0;
+            leng = 0;
+            color = Color.Empty;
+            th1 = 0;
+            th2 = 0;
+            per1 = 0;
+            per2 = 0;
+            textBox2.Clear();
+            textBox9.Clear();
+            textBox10.Clear();
+            textBox11.Clear();
+            textBox12.Clear();
+            textBox13.Clear();
+            panel1.Refresh();
+        }
+    }
+}
